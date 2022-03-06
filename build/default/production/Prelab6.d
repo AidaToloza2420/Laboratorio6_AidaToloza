2a8c3fb1f98fc12a4079b4ceec07fecf7fe29db5
Aida Toloza <>
2022-03-05 21:34:16 -0600

    PostLab6_Final

---
+++ build/default/production/Prelab6.d
@@ -0,0 +1,1 @@
+build/default/production\Prelab6.i: Prelab6.s
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Lab6_Sim.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Lab6_Sim.X.production.hex
